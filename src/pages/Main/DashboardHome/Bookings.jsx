6957a6f0f1d879e6759077614b0a94cbdbf527a0
Raiyan109 <kraiyan109@gmail.com>
2025-01-28 18:12:44 +0600

    calendar page started

--- src/pages/Main/DashboardHome/Bookings.jsx
+++ src/pages/Main/DashboardHome/Bookings.jsx
@@ -1,9 +1,12 @@
+import { getMonth } from "../../../utils/getMonth"
 
 
 const Bookings = () => {
+    console.table(getMonth())
     return (
-        <div>
-
+        <div className="flex h-screen">
+            <div className="w-1/4 bg-pink-400"></div>
+            <div className="w-3/4 bg-purple-400"></div>
         </div>
     )
 }
